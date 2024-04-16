7148e16ad9c21f732113eb7040dbb27d80a190a2
Huzaifaabdulrab <huzaifaabdulrab2@gmail.com>
2024-04-17 02:52:11 +0500

    Word-Counter
    TS/JS Word counter created a using inquirer & chalk

---
+++ index.ts
@@ -0,0 +1,37 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+// Initialize the operation flag to false to enter the loop
+let operation: boolean = false;
+
+// Loop until operation becomes true
+while (!operation) {
+    // Prompt the user to enter a sentence
+    const answer: { sentence: string } = await inquirer.prompt({
+        name: "sentence",
+        type: "input",
+        message: chalk.cyan(`Enter your Sentence to count the number`)
+    });
+
+    // Get the sentence entered by the user
+    const sentence = answer.sentence;
+
+    // Check if the user entered a sentence
+    if (!sentence) {
+        // If no sentence is entered, display an error message
+        console.log(chalk.redBright("\nError: Please enter a sentence."));
+    } else {
+        // If a sentence is entered, split it into words
+        const wordArray = sentence.trim().split(" ");
+
+        // Display the words in the sentence
+        console.log();
+        console.log(wordArray);
+
+        // Display the count of words in the sentence
+        console.log(chalk.yellow(`\nYour sentence word count is ${wordArray.length}`));
+
+        // Set operation to true to exit the loop
+        operation = true;
+    }
+}
